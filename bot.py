94910cf9c449a756db8d7ab55001852d03bb99d4
Raj Dave <10864@bpsdoha.edu.qa>
2022-10-19 18:42:39 +0300

    create page list command

--- bot.py
+++ bot.py
@@ -45,7 +45,73 @@ def _latest(update, context):
     update.message.reply_photo(png, caption=reply_text, parse_mode="Markdown")
 
 
+def _list(update, context):
+    """Send the list of circulars."""
+    category = context.args[0]
+    raw_res = get_circular_list(category)
+    if raw_res is None:
+        update.message.reply_text("Error in fetching list of circulars.")
+        return
+    reply_text = ""
+    reply_text += f"List of `{category}` Circulars:\n\n"
 
+    loop_int = 1
+    links = []
+    titles = []
+
+    for item in raw_res:
+        titles.append(f"*{loop_int}*. `{item['title']}`")  # Add the title to the list
+        links.append(f"{item['link']}")  # Add the link to the list
+        loop_int += 1
+
+    final_data = []
+    temp = []
+
+    count = 0
+
+    for title, link in zip(titles, links):
+        temp.append(f"{title}\n {link}\n\n")
+        count += 1
+        if count % 10 == 0:  # If the count is divisible by 10 (It has reached 10 fields)
+            final_data.append(''.join(temp))  # Create a copy of the embed and add it to the list
+            temp.clear()
+        elif count == len(titles):
+            final_data.append(''.join(temp))
+            temp.clear()
+
+    paginator = InlineKeyboardPaginator(
+        len(titles) // 10 + 1,
+        data_pattern='character#{page}'
+    )
+
+    # send the message with the paginator
+    update.message.reply_text(
+        text=final_data[0],
+        reply_markup=paginator.markup,
+        parse_mode='Markdown'
+    )
+    global page_list
+    page_list = final_data
+
+
+def list_page_callback(update, context):
+    query = update.callback_query
+
+    query.answer()
+
+    page = int(query.data.split('#')[1])
+
+    paginator = InlineKeyboardPaginator(
+        len(page_list),
+        current_page=page,
+        data_pattern='character#{page}'
+    )
+
+    query.edit_message_text(
+        text=page_list[page - 1],
+        reply_markup=paginator.markup,
+        parse_mode='Markdown'
+    )
 
 
 def error(update, context):
@@ -61,13 +127,13 @@ def main():
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
-    # dp.add_handler(CallbackQueryHandler(list_page_callback, pattern='^character#'))
+    dp.add_handler(CallbackQueryHandler(list_page_callback, pattern='^character#'))
 
     # Command Handlers
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
     dp.add_handler(CommandHandler("latest", _latest))
-    # dp.add_handler(CommandHandler("list", _list))
+    dp.add_handler(CommandHandler("list", _list))
 
     dp.add_error_handler(error)
 
